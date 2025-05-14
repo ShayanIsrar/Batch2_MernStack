086f087fde01188ebbd86905e36971cadb62ad92
ShayanIsrar <shayanisrar19@gmail.com>
2025-05-14 01:32:46 -0700

    something else
    somthing else

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ColorPicker from "./components/ColorPicker";
-// import Counter from "./components/Counter";
+// import ColorPicker from "./components/ColorPicker";
+import Counter from "./components/Counter";
 // import MapMethod from "./components/MapMethod";
 
 const App = () => {
   return (
     <div>
       {/* <MapMethod /> */}
-      {/* <Counter /> */}
-      <ColorPicker />
+      <Counter />
+      {/* <ColorPicker /> */}
     </div>
   );
 };
